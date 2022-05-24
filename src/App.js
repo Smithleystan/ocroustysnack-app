fe6605598776bd3414da81b3b96e0a152232d52b
smithleystan <stan.smithley@gmail.com>
2022-05-24 09:06:48 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import "./App.css";
-import { Routes, Route, Router } from "react-router-dom";
-import Home from "./Pages/Home";
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Repas from"./Pages/Carterepas"
-import Gouter from "./Pages/Gouter"
-import Contact from "./Pages/Contact"
-import Plan from "./Pages/Plan"
-import Boisson from "./Pages/Boisson"
-import Menu from "../src/Pages/Menu"
-import {useState} from "react"
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  
   return (
-    
-
-    <>
-    <div><Navbar/></div>
-    
-      <Routes basename={process.env.PUBLIC_URL} >
-      <Route path="" element = {<Home />}/>
-      <Route path="/Plan" element = {<Plan />}/>
-      <Route path="/Contact" element = {<Contact />}/>
-      <Route path="/Gouter" element = {<Gouter />}/>
-      <Route path="/Repas" element = {<Repas />}/>
-      <Route path="/Boisson" element = {<Boisson />}/>
-      </Routes>
-    <div><Footer/></div>
-    
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
