a79dd3ba2629b06aaaf5662eee5fb7111ef51a32
smithleystan <stan.smithley@gmail.com>
2022-05-24 10:24:19 +0200

    Navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../App.css"
+
+function Navbar() {
+  return (
+      <>
+      <nav className='navbar'>
+     <ul>
+         <li>Accueil</li>
+         <li>Cartes</li>
+         <li>restaurant & vous</li>
+         <li>Nous trouver</li>
+         <li>Logo restaurant</li>
+     </ul>
+     </nav>
+   
+    </>
+  )
+}
+
+export default Navbar
