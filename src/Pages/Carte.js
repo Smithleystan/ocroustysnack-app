a0114cbf02259fcd802ab8cd9d3835d01f6faac3
stanislas Nakavua <stan.smithley@gmail.com>
2022-06-18 10:00:18 +0200

    test

--- src/Pages/Carte.js
+++ src/Pages/Carte.js
@@ -7,7 +7,7 @@ function Carte() {
           <hr/>
           <div className='brefmaster'>
          <div className='bref'>
-            <Link to="../sandwitchs"><div className='imgnav imgm1'><h1>Sandwichs</h1></div></Link>
+            <Link to="../sandwitchs"><div className= 'imgnav imgm1'><h1>Sandwichs</h1></div></Link>
             <Link to="../burger"><div className='imgnav imgm2'><div><div><h1>Burgers</h1></div><div><h1>&</h1></div><div><h1>Galettes</h1></div></div></div></Link>
             <Link to="../chickens"><div className='imgnav imgm3'><h1>Chickens</h1></div></Link>
             <Link to="../extra"><div className='imgnav imgm4'><h1>Extras</h1> </div></Link>
