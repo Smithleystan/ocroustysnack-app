a13dc09a4c92b193f4ad59e17b01f61c99388b52
stanislas <stan.smithley@gmail.com>
2022-07-11 16:52:09 +0200

    btn

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,22 +8,23 @@ import {useState} from "react"
 function Navbar() {
  const [hid,setHid] = useState(false)
  const btn = () => setHid(!hid)
+
  
 
   return (
       <>
     <nav className="nav">
     
-   <div className='btncenter' onClick={btn}>
+   <div className='btncenter' onClick={btn} on>
   <img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1655764496/ocroustycard/aligner-justifier_qgdx5j.png" className="btnbarre" ></img>
   </div>
   
-      <div className={hid? "navcenter" :"navcenterHidden"}>
-          <Link to="/" className="li"><div ><h3>Accueil</h3></div></Link>
-          <Link to="carte"  className="li"><div ><h3>Cartes</h3></div></Link>
-          <Link to="horaire" className="li"><div ><h3>Horaires</h3></div></Link>
-          <Link to="place" className="li"><div ><h3>Nous trouver</h3></div></Link>
-          <Link to="/" className=" logoposition"><div ><h3><img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1654940881/ocroustycard/Croustylogo_skanit.jpg" className="logocrousty"/></h3></div></Link>
+      <div className={hid? "navcenter" :"navcenterHidden navcenter"}>
+          <Link to="/" className="li"><div onClick={btn}><h3>Accueil</h3></div></Link>
+          <Link to="carte"  className="li"><div onClick={btn} ><h3>Cartes</h3></div></Link>
+          <Link to="horaire" className="li"><div  onClick={btn}><h3>Horaires</h3></div></Link>
+          <Link to="place" className="li"><div onClick={btn}><h3>Nous trouver</h3></div></Link>
+          <Link to="/" className=" logoposition"><div onClick={btn} ><h3><img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1654940881/ocroustycard/Croustylogo_skanit.jpg" className="logocrousty"/></h3></div></Link>
       </div>
      </nav>
 
