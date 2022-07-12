e2b9810b25242379ec17c0172a38a1160b519b3a
stanislas <stan.smithley@gmail.com>
2022-07-12 17:15:11 +0200

    responsive

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -13,9 +13,17 @@ function Navbar() {
   return (
       <>
     <nav className="nav">
-    
-   <div className='btncenter' onClick={btn} on>
-  <img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1655764496/ocroustycard/aligner-justifier_qgdx5j.png" className="btnbarre" ></img>
+  {/* <div className="btnlogo">
+    <div className='btncenter' onClick={btn} on>
+    <img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1655764496/ocroustycard/aligner-justifier_qgdx5j.png" className="btnbarre" ></img>
+    </div>
+    { <div className="logo">
+    <img img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1654940881/ocroustycard/Croustylogo_skanit.jpg" className="logocrousty"></img>
+    </div> }
+  </div> */}
+  <div className="croustyetbtn">
+    <div onClick={btn}><img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1655764496/ocroustycard/aligner-justifier_qgdx5j.png" className="btn"></img></div>
+    <Link to="/"><div><img img src="https://res.cloudinary.com/dsx6152nt/image/upload/v1654940881/ocroustycard/Croustylogo_skanit.jpg" className="logo"></img></div></Link>
   </div>
   
       <div className={hid? "navcenter left":"navcenterHidden navcenter"}>
